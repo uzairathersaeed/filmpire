a32fac7a9f2002e9c1a5354254776a5e0bbf1c2b
UZAIR ATHER <uzairathersaeed>
2023-04-09 21:09:32 +0500

    File and folder structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
-
-const App = () => (
-  <div>
-    App
-  </div>
-);
-
+import { Routes, Route } from "react-router-dom";
+import { CssBaseline } from '@mui/material';
+import {Actors, Movies, NavBar, MovieInformation, Profile } from './index'
+import useStyles from './styles';
+const App = () => {
+ const classes = useStyles();
+ return(
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+        <main className={classes.content}>
+        <div className={classes.toolbar}/>
+          <Routes>
+            <Route  path="/movie/:id" element={<MovieInformation />}>
+            </Route>
+            <Route  path="/actors/:id" element={<Actors/>}>
+            </Route>
+            <Route  path="/profile/:id" element={<Profile/>}>
+            </Route>
+            <Route  path="/" element={
+              <Movies />
+            }>
+            </Route>
+          </Routes>
+        </main>
+    </div>
+  );
+};
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
-
-import ReactDOM from 'react-dom';
-
+import ReactDOM  from "react-dom/client";
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
+const el = document.getElementById('root');
+const root = ReactDOM.createRoot(el);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
